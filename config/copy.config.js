233c40d8a548415c6254b03e57fbeae5670f3393
Pau Ferrer Ocaña <crazyserver@gmail.com>
2020-09-18 12:37:08 +0200

    package.json

--- config/copy.config.js
+++ config/copy.config.js
@@ -1,5 +1,25 @@
 // New copy task for font files and config.json.
 module.exports = {
+    copyAssets: {
+    src: ['{{SRC}}/assets/**/*'],
+    dest: '{{WWW}}/assets'
+  },
+  copyIndexContent: {
+    src: ['{{SRC}}/index.html', '{{SRC}}/manifest.json', '{{SRC}}/service-worker.js'],
+    dest: '{{WWW}}'
+  },
+  copyFonts: {
+    src: ['{{ROOT}}/node_modules/ionicons/dist/fonts/**/*', '{{ROOT}}/node_modules/ionic-angular/fonts/**/*'],
+    dest: '{{WWW}}/assets/fonts'
+  },
+  copyPolyfills: {
+    src: [`{{ROOT}}/node_modules/ionic-angular/polyfills/${process.env.IONIC_POLYFILL_FILE_NAME}`],
+    dest: '{{BUILD}}'
+  },
+  copySwToolbox: {
+    src: ['{{ROOT}}/node_modules/sw-toolbox/sw-toolbox.js'],
+    dest: '{{BUILD}}'
+  },
   // Override Ionic copyFonts task to exclude Roboto and Noto fonts.
   copyFonts: {
     src: ['{{ROOT}}/node_modules/ionicons/dist/fonts/**/*'],
@@ -42,7 +62,7 @@ module.exports = {
     dest: '{{WWW}}/lib/mathjax/localization'
   },
   copyH5P: {
-    src: ['{{ROOT}}/src/core/h5p/assets/**/*'],
+    src: ['{{ROOT}}/src/appcore/h5p/assets/**/*'],
     dest: '{{WWW}}/h5p/'
   },
 };
