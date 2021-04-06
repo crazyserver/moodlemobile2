7bd8361c146014e1ba5bfd580a3861840f13350d
Pau Ferrer Ocaña <crazyserver@gmail.com>
2021-04-06 16:21:30 +0200

    v3.5.3

--- v3.5.ts
+++ v3.5.ts
@@ -1346,6 +1346,8 @@ export type CoreCourseGetContentsWSParams = {
         name: string; // The expected keys (value format) are:
                                              // excludemodules (bool) Do not return modules, return only the sections structure
                                              // excludecontents (bool) Do not return module contents (i.e: files inside a resource)
+                                             // includestealthmodules (bool) Return stealth modules for students in a special
+                                                 // section (with id -1)
                                              // sectionid (int) Return only this section
                                              // sectionnumber (int) Return only this section with number (order)
                                              // cmid (int) Return only this module information (among the whole sections structure)
